f9f0d3d225a5518a9dbb65c6929bd11ca4ed89e7
oidex69 <shresthaprince222@gmail.com>
2026-04-02 15:16:51 +0545

    added self ping

---
+++ src/main/java/zpt/dsaVis/selfping.java
@@ -0,0 +1,16 @@
+package zpt.dsaVis;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class selfping {
+    private final RestTemplate rstTmp = new RestTemplate();
+
+    @Scheduled(fixedRate = 50000)
+    public void ping() {
+        System.out.println("refreshed");
+        rstTmp.getForObject("https://portfolio-gfky.onrender.com/",String.class);
+    }
+}
